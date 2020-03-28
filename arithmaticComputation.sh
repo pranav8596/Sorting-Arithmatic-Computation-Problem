4aa48ba2130a8adad903ac80b2a0d730ce04d2ba
pranav8596 <pige786123@gmail.com>
2020-03-29 01:46:02 +0530

    ADDED: Computed the expreesion a % b + c

--- arithmaticComputation.sh
+++ arithmaticComputation.sh
@@ -5,3 +5,4 @@ read -p "Enter the values of a, b & c : " a b c
 result1=`echo "scale=2; $(($a + $b * $c ))" | bc`
 result2=`echo "scale=2; $(($a * $b + $c ))" | bc`
 result3=`echo "scale=2; $(($c + $a / $b ))" | bc`
+result4=`echo "scale=2; $(($a % $b + $c ))" | bc`
