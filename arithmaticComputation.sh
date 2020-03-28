3725e3a1c24c54bfffc25d30f03af017a5a21e95
pranav8596 <pige786123@gmail.com>
2020-03-29 01:42:55 +0530

    ADDED: Computed the expreesion c * a / b

--- arithmaticComputation.sh
+++ arithmaticComputation.sh
@@ -4,3 +4,4 @@ read -p "Enter the values of a, b & c : " a b c
 
 result1=`echo "scale=2; $(($a + $b * $c ))" | bc`
 result2=`echo "scale=2; $(($a * $b + $c ))" | bc`
+result3=`echo "scale=2; $(($c + $a / $b ))" | bc`
